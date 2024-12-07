5af06c0d284fd25ceac35e66b5a11e20f15c7608
Webprojon <tokhirjonkhasanov@gmail.com>
2024-12-07 22:11:07 +0100

    accardion is added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -42,17 +42,23 @@ export default function Header() {
 	];
 
 	return (
-		<header className="relative md:border-b border-slate-500 text-white tracking-wider">
-			<div
-				onClick={() => setMenu(!menu)}
-				className="absolute right-4 top-3 z-10 md:hidden"
-			>
-				{menu ? <IoClose className="size-9" /> : <MdMenu className="size-9" />}
+		<header className="bg-[#20293adf] opacity-60 h-[10vh] relative md:border-b border-slate-600 text-white tracking-wider">
+			<div>
+				<div
+					onClick={() => setMenu(!menu)}
+					className="absolute right-4 top-3 z-10 md:hidden"
+				>
+					{menu ? (
+						<IoClose className="size-9" />
+					) : (
+						<MdMenu className="size-9" />
+					)}
+				</div>
 			</div>
 
 			<nav
 				className={`absolute md:static pt-5 px-2 md:pt-0 flex flex-col gap-y-6 md:flex-row justify-between items-center md:h-[12vh]
-					z-0 lg:max-w-[1350px] mx-auto transition-transform duration-500
+					z-0 lg:max-w-[1200px] mx-auto transition-transform duration-500
 					 ${menu ? "translate-y-0" : "-translate-y-full md:translate-y-0"} md:flex`}
 			>
 				<Link to="/" className="font-semibold text-[20px] uppercase">
@@ -70,14 +76,6 @@ export default function Header() {
 							{link.link}
 						</Link>
 					))}
-				</div>
-
-				<div className="cursor-pointer my-4 text-[17px]">
-					<span className="py-[4px] px-2 bg-slate-700">EN</span>
-					<span className="py-[4px] px-4 border-r border-l border-slate-400">
-						RU
-					</span>
-					<span className="py-[4px] px-2">UZ</span>
 				</div>
 			</nav>
 		</header>
